9ed5025abf664a4a1c72796581f938f642d4c606
snow <136385621+snowcloudsx@users.noreply.github.com>
2025-05-31 18:01:03 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,13 @@
 self.addEventListener("install", (e) => {
   console.log("Service Worker installed");
-  e.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./main.js",
-        "./manifest.json",
-        "./settings_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png"
+  self.skipWaiting();
+});
 
-      ]);
-    })
-  );
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker activated");
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  // Basic passthrough fetch handler
+  e.respondWith(fetch(e.request));
 });
